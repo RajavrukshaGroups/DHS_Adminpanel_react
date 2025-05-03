4d7ffa25a603aa97a3dbe7838fa97467abcbad3f
Sabeeh KK <sabeehkadungalloor573@gmail.com>
2025-05-03 09:55:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <span className='bg-amber-600'>DHS ADMIN PANEL</span>
+    </>
+  )
+}
+
+export default App

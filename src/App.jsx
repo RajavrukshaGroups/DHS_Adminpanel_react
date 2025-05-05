5a21eaab55fab342a6a90cab578374950da4d432
Sabeeh KK <sabeehkadungalloor573@gmail.com>
2025-05-05 09:41:47 +0530

    fixed tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    <h1 class="text-3xl font-bold underline">
+    <h1 class="text-3xl font-bold underline bg-amber-300">
       DHS ADMIN PANEL
     </h1>
     </>

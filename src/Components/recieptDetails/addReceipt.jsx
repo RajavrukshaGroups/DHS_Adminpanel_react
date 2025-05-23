4108763170026a15cc9117d27faaf13934c1eeb4
RajavrukshaGroups <technical@rajavrukshagroup.in>
2025-05-23 10:42:38 +0530

    minor

--- src/Components/recieptDetails/addReceipt.jsx
+++ src/Components/recieptDetails/addReceipt.jsx
@@ -25,7 +25,7 @@ const AddReceipt = () => {
     ddNumber: "",
   });
 
-  console.log("members data", membersData);
+  console.log("members dataeyyeyteyrttr", membersData);
 
   useEffect(() => {
     const fetchMember = async () => {

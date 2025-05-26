f4cae2e39154895add06f329e113f0f85586c130
Sabeeh KK <sabeehkadungalloor573@gmail.com>
2025-05-26 15:53:03 +0530

    fixed side bar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -318,7 +318,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                     to="/plotTransferForm"
                     className={`flex items-center px-3 py-2 rounded-lg ${
                       // location.pathname === "/addreferencedetails"
-                      location.pathname === "/viewsiteBooking"
+                      location.pathname === "/plotTransferForm"
                         ? "bg-blue-100 text-blue-700"
                         : "text-black hover:bg-gray-100"
                     }`}
@@ -331,7 +331,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                     to="/plotTransferhistory"
                     className={`flex items-center px-3 py-2 rounded-lg ${
                       // location.pathname === "/addreferencedetails"
-                      location.pathname === "/viewsiteBooking"
+                      location.pathname === "/plotTransferhistory"
                         ? "bg-blue-100 text-blue-700"
                         : "text-black hover:bg-gray-100"
                     }`}

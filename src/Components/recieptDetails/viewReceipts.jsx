57bd6dcf4a3cbcfa9d2ab24e526c7bbfc144d6d1
Sabeeh KK <sabeehkadungalloor573@gmail.com>
2025-06-25 14:03:43 +0530

    fixed reciept issues in the amdinpanel reciept fetching

--- src/Components/recieptDetails/viewReceipts.jsx
+++ src/Components/recieptDetails/viewReceipts.jsx
@@ -15,7 +15,7 @@ const ViewReceiptDetails = () => {
     try {
       const res = await axios.get(
         // `http://localhost:4000/receipt/get-receipt-details`,
-        `https://adminpanel.defence.housingsociety.com/receipt/get-receipt-details`,
+        `https://adminpaneldefence.housingsociety.com/receipt/get-receipt-details`,
         {
           params: {
             page,
